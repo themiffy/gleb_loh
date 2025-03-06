454095fe3c57546cdbae3a97487dfccd7828a07f
miffy <specialfnp@gmail.com>
2025-03-06 09:51:07 +0300

    Version 2

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 import logging
-from datetime import datetime
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 
-# Настройка логгера для сообщений
-logging.basicConfig(
-    filename='messages.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(message)s'
+# Настройка отдельного логгера для сообщений
+message_logger = logging.getLogger('messages')
+message_logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(
+    'messages.log',
+    maxBytes=10000,
+    backupCount=1,
+    encoding='utf-8'
 )
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+handler.setFormatter(formatter)
+message_logger.addHandler(handler)
 
 @app.route('/')
 def index():
@@ -18,9 +25,8 @@ def index():
 @app.route('/log_message', methods=['POST'])
 def log_message():
     data = request.json
-    log_entry = f"Сообщение от {data['name']}: {data['message']}"
-    app.logger.info(log_entry)
+    message_logger.info(f"{data['name']}::: {data['message']}")
     return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001)
